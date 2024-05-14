02283a40c262b45e1af3829c55f638b9bd081a2a
Yuya Oda <yoda@c2r6s9.42tokyo.jp>
2024-05-14 19:43:50 +0900

    fixed getDiscordIdFromName()

--- src/utils/login-to-mention.ts
+++ src/utils/login-to-mention.ts
@@ -2,7 +2,7 @@ import { Collection, GuildMember } from 'discord.js';
 
 function getDiscordIdFromName(login: string, members: Collection<string, GuildMember>): string | void {
 	const member = members.find(member => { return
-		member.displayName === login || member.globalName === login;
+		member.displayName === login || member.user.globalName === login;
 	});
 	if (!member) {
 		return;
