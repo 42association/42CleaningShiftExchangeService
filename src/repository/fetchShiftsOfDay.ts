f1f62f2748c54e58b66bffbafb468ab30ec232c3
Yuya Oda <yoda@c4r2s12.42tokyo.jp>
2024-05-14 15:58:32 +0900

    fixed query-param to API

--- src/repository/fetchShiftsOfDay.ts
+++ src/repository/fetchShiftsOfDay.ts
@@ -3,8 +3,16 @@ import { ErrorMessage } from "../types/error-message";
 
 const endpoint = "/shifts"
 
+function padding(num: number): string {
+	return num < 10 ? `0${num}` : num.toString();
+}
+
+function dateToString(date: Date): string {
+	return `${date.getFullYear()}-${padding(date.getMonth() + 1)}-${padding(date.getDate())}`;
+}
+
 export async function FetchShiftsOfDay(day: Date): Promise<ShiftList | void> {
-	const dateStr = `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`;
+	const dateStr = dateToString(day);
 	const url = `${process.env.API_URL}${endpoint}?date=${dateStr}`;
 	const response = await fetch(url, {
 		method: "GET",
